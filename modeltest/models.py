03215da172358028de03cb9e7415bd0bb3e5fdd5
daniel4191 <daniell8282@naver.com>
2025-08-11 23:47:35 +0900

    models추가에 admin 추가까지

--- modeltest/models.py
+++ modeltest/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField("제목",max_length=30)
+    content = models.TextField("내용")
+    
+    def __str__(self):
+        return f"{self.title}: {self.content}"
