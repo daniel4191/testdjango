ffe3cf71187fbde8240c5b23fece94ea56149cf8
daniel4191 <daniell8282@naver.com>
2025-08-11 23:56:07 +0900

    댓글기능 추가 후 테스트완료

--- modeltest/admin.py
+++ modeltest/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from modeltest.models import Post
+from modeltest.models import Post, Comment
 
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
     pass
--- modeltest/models.py
+++ modeltest/models.py
@@ -7,3 +7,10 @@ class Post(models.Model):
     
     def __str__(self):
         return f"{self.title}: {self.content}"
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name="선택할 포스트")
+    comment = models.TextField("댓글내용")
+    
+    def __str__(self):
+        return f"선택한 포스트:{self.post} / 댓글내용: {self.comment}"
